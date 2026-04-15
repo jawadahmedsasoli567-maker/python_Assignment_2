b812f133588d8bec2f0d962f2ba40b264cc7197f
jawadahmedsasoli567-maker <jawadahmedsasoli567@gmail.com>
2026-04-15 14:31:21 +0500

    Add files via upload

---
+++ Assigment2.py
@@ -0,0 +1,18 @@
+name=input("Enter your name")
+city=input("Enter your city")
+subject=input("Enter your favorit subject")
+print("Hello",name)
+print("You Live in",city,"and Your favorit subject is",subject)
+
+word1=input("Enter first word")
+word2=input("Enter second word")
+
+print(word1 + word2)
+
+username=input("Enter username")
+print(username == "Admin")
+
+sentence= "I am learning python programming"
+word=input("enter a word")
+
+print(word in sentence)
